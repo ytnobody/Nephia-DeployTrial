6f88018663946feb00878e4a0a5b86539d7c9a31
ytnobody / satoshi azuma <ytnobody@gmail.com>
2012-08-10 16:44:46 +0900

    init

---
+++ lib/Nephia/DeployTrial.pm
@@ -0,0 +1,44 @@
+package Nephia::DeployTrial;
+use strict;
+use warnings;
+use Nephia;
+
+our $VERSION = 0.01;
+
+path '/' => sub {
+    my $req = shift;
+    return {
+        template => 'index.tx',
+        title => 'Nephia::DeployTrial',
+    };
+};
+
+1;
+__END__
+
+=head1 NAME
+
+Nephia::DeployTrial - Web Application
+
+=head1 SYNOPSIS
+
+  $ plackup
+
+=head1 DESCRIPTION
+
+Nephia::DeployTrial is web application based Nephia.
+
+=head1 AUTHOR
+
+clever guy
+
+=head1 SEE ALSO
+
+Nephia
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
